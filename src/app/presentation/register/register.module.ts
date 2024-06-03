f13d89917dfa5e3656d00e6a6d8b7c9463a27085
Sergio <sergio@MacBook-Pro.local>
2024-06-03 16:48:00 -0700

    feat: implement module register and usewr service.. configs

--- src/app/presentation/register/register.module.ts
+++ src/app/presentation/register/register.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,6 +8,10 @@ import { RegisterPageRoutingModule } from './register-routing.module';
 
 import { RegisterPage } from './register.page';
 import { SharedModule } from '../shared/shared.module';
+import { StepOneComponent } from './components/step-one/step-one.component';
+import { StepTwoComponent } from './components/step-two/step-two.component';
+import { StepThreeComponent } from './components/step-three/step-three.component';
+import { HeaderStepsComponent } from './components/header-steps/header-steps.component';
 
 @NgModule({
   imports: [
@@ -17,6 +21,13 @@ import { SharedModule } from '../shared/shared.module';
     RegisterPageRoutingModule,
     SharedModule,
   ],
-  declarations: [RegisterPage],
+  declarations: [
+    RegisterPage,
+    StepOneComponent,
+    StepTwoComponent,
+    StepThreeComponent,
+    HeaderStepsComponent,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class RegisterPageModule {}
--- src/app/presentation/register/register.page.ts
+++ src/app/presentation/register/register.page.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import Swiper from 'swiper';
+import { register } from 'swiper/element/bundle';
+import { UserService } from '../services/user.service';
+import { RegisterEntity } from 'src/app/domain';
+import { Router } from '@angular/router';
+
+register();
 
 @Component({
   selector: 'app-register',
@@ -6,10 +14,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+  currentIndex: string;
+  headerText: string;
 
-  constructor() { }
+  userData!: RegisterEntity;
 
-  ngOnInit() {
+  slideOpts = {
+    allowTouchMove: false,
+    allowSlideNext: false,
+    allowSlidePrev: false,
+  };
+
+  constructor(private router: Router) {
+    this.currentIndex = '0';
+    this.headerText = 'NÚMERO CELULAR';
   }
 
+  ngOnInit() {}
+
+  goBack() {
+    switch (this.currentIndex) {
+      case '0':
+        this.router.navigate(['/']);
+        break;
+      case '1':
+      case '2':
+        this.swiperRef?.nativeElement.swiper.slidePrev();
+        break;
+      default:
+        break;
+    }
+  }
+
+  statusChanged(index: string) {
+    switch (index) {
+      case '0':
+        this.headerText = 'NÚMERO CELULAR';
+        break;
+      case '1':
+        this.headerText = 'DATOS DE CUENTA';
+        break;
+      case '2':
+        this.headerText = 'FINALIZAR';
+        break;
+    }
+  }
+
+  swiperSlideChanged(e: any) {
+    this.currentIndex =
+      this.swiperRef?.nativeElement.swiper.activeIndex.toString();
+    this.statusChanged(this.currentIndex);
+  }
+
+  actionStepOne(phone: string) {
+    this.userData = {
+      ...this.userData,
+      phone,
+    };
+
+    this.swiperRef?.nativeElement.swiper.slideNext();
+  }
+
+  actionStepTwo(data: RegisterEntity) {
+    this.userData = {
+      ...this.userData,
+      ...data,
+    };
+    this.swiperRef?.nativeElement.swiper.slideNext();
+  }
+
+  actionStepThree(terms: boolean) {
+    this.userData = {
+      ...this.userData,
+      terms,
+    };
+
+    this.router.navigate(['/home']);
+    console.log('userData', this.userData);
+  }
 }
